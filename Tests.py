9585aa3f20e5d5e94d779293a8ef0ec4791683d8
Pushkin Sergey <psvfreeman@gmail.com>
2019-09-24 11:49:07 +0300

    First_result

---
+++ Tests.py
@@ -0,0 +1,12 @@
+print("OK")
+import re
+snl=[]
+with open("C:\\Repository\python.ry") as f:
+	while True:
+			s = f.readline()
+			if s== "":
+				break
+			sn = re.sub('^\s{2,5}',' '*4,s)
+			snl += [sn]
+print (snl)
+
