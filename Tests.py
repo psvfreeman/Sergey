7be80d134a62fb0fbf18b7fda28ab3af41e65624
Pushkin Sergey <psvfreeman@gmail.com>
2019-09-24 12:05:51 +0300

    First_result1

--- Tests.py
+++ Tests.py
@@ -9,4 +9,7 @@ with open("C:\\Repository\python.ry") as f:
 			sn = re.sub('^\s{2,5}',' '*4,s)
 			snl += [sn]
 print (snl)
+with open ("C:\\Repository\python.ry","w") as f:
+    f.writelines(snl)
+    print(snl)
 
